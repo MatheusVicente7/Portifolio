c691c362a39541e75fdd9ebe11bbec505ff34299
MatheusVicente7 <44214210+MatheusVicente7@users.noreply.github.com>
2021-06-29 16:23:48 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://matheusvicente7.github.io/Portifolio/`,
     // Your Name
     name: 'Ryan Fitzgerald',
     // Main Site Title
